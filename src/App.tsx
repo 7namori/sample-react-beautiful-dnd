06bd79928f48fe0fcbb5c08949c106584d4f057d
nanamori <97284120+7namori@users.noreply.github.com>
2022-02-04 15:54:50 +0900

    [update]ドロップ対象の範囲を追加 ドラッグしたアイテムをドロップするための範囲をDroppableで囲うことで指定する。

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,35 @@
 import './styles.css'
 import { dummyData, dummyDataType } from './dummyData'
 import { Box, Grid } from '@mui/material'
-import { DragDropContext } from 'react-beautiful-dnd'
+import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 
 export default function App() {
   return (
     <div className="App">
       <h1>react-beautiful-dnd-sample</h1>
       <DragDropContext onDragEnd={() => console.log('onDragEnd')}>
-        <Grid container spacing={2}>
-          {dummyData.map((values: dummyDataType, idx: number) => {
-            return (
-              <Grid key={idx} item xs={4}>
-                <Box
-                  sx={{
-                    color: 'primary.contrastText',
-                    backgroundColor: 'primary.main'
-                  }}
-                >
-                  {values.val}
-                </Box>
+        <Droppable droppableId="gridItems">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              <Grid container spacing={2}>
+                {dummyData.map((values: dummyDataType, idx: number) => {
+                  return (
+                    <Grid key={idx} item xs={4}>
+                      <Box
+                        sx={{
+                          color: 'primary.contrastText',
+                          backgroundColor: 'primary.main'
+                        }}
+                      >
+                        {values.val}
+                      </Box>
+                    </Grid>
+                  )
+                })}
               </Grid>
-            )
-          })}
-        </Grid>
+            </div>
+          )}
+        </Droppable>
       </DragDropContext>
     </div>
   )
