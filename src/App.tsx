3e4118830ca4184e4b04b6328f2402b4386635bb
nanamori <97284120+7namori@users.noreply.github.com>
2022-02-04 15:44:36 +0900

    [update]DragDropContextを追加 DragDropContextでコンポーネントをラップすることで、ライブラリが配下のコンポーネントツリーにアクセスできるようになる。

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
 import './styles.css'
 import { dummyData, dummyDataType } from './dummyData'
 import { Box, Grid } from '@mui/material'
+import { DragDropContext } from 'react-beautiful-dnd'
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <Grid container spacing={2}>
-        {dummyData.map((values: dummyDataType, idx: number) => {
-          return (
-            <Grid key={idx} item xs={4}>
-              <Box
-                sx={{
-                  color: 'primary.contrastText',
-                  backgroundColor: 'primary.main'
-                }}
-              >
-                {values.val}
-              </Box>
-            </Grid>
-          )
-        })}
-      </Grid>
+      <h1>react-beautiful-dnd-sample</h1>
+      <DragDropContext onDragEnd={() => console.log('onDragEnd')}>
+        <Grid container spacing={2}>
+          {dummyData.map((values: dummyDataType, idx: number) => {
+            return (
+              <Grid key={idx} item xs={4}>
+                <Box
+                  sx={{
+                    color: 'primary.contrastText',
+                    backgroundColor: 'primary.main'
+                  }}
+                >
+                  {values.val}
+                </Box>
+              </Grid>
+            )
+          })}
+        </Grid>
+      </DragDropContext>
     </div>
   )
 }
