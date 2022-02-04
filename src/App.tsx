444c1f5f58ccc68decf548b2725d7ce00dbc98ae
nanamori <97284120+7namori@users.noreply.github.com>
2022-02-04 16:33:58 +0900

    [update]stateを使用して変更された順番を保持する処理を追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,27 @@ import './styles.css'
 import { dummyData, dummyDataType } from './dummyData'
 import { Box, Grid } from '@mui/material'
 import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd'
+import { useState } from 'react'
 
 export default function App() {
+  const [gridItems, setGridItems] = useState(dummyData)
+  const handleOnDragEnd = (result: any) => {
+    const items = Array.from(gridItems)
+    const [reorderedItems] = items.splice(result.source.index, 1)
+    items.splice(result.destination.index, 0, reorderedItems)
+
+    setGridItems(items)
+  }
+
   return (
     <div className="App">
       <h1>react-beautiful-dnd-sample</h1>
-      <DragDropContext onDragEnd={() => console.log('onDragEnd')}>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
         <Droppable droppableId="gridItems">
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
               {/* <Grid container spacing={2}> */}
-              {dummyData.map((values: dummyDataType, idx: number) => {
+              {gridItems.map((values: dummyDataType, idx: number) => {
                 return (
                   <Draggable
                     draggableId={values.id}
