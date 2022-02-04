cf8f4019c6fadfe4a3103bbaeef0f0652748698c
nanamori <97284120+7namori@users.noreply.github.com>
2022-02-04 14:14:31 +0900

    [update]表示したデータに背景色を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import './styles.css'
 import { dummyData, dummyDataType } from './dummyData'
-import { Grid } from '@mui/material'
+import { Box, Grid } from '@mui/material'
 
 export default function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <Grid container>
+      <Grid container spacing={2}>
         {dummyData.map((values: dummyDataType, idx: number) => {
           return (
             <Grid key={idx} item xs={4}>
-              {values.val}
+              <Box
+                sx={{
+                  color: 'primary.contrastText',
+                  backgroundColor: 'primary.main'
+                }}
+              >
+                {values.val}
+              </Box>
             </Grid>
           )
         })}
